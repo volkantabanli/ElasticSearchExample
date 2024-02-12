58557b7ef711739279b8f24869fe42d5bf7c493a
volkantabanli <vlkntbnl@gmail.com>
2024-02-12 23:00:45 +0300

    Proje oluşturuldu

---
+++ ElasticSearchExample/Controllers/ItemsController.cs
@@ -0,0 +1,67 @@
+﻿using ElasticSearchExample.Dal;
+using ElasticSearchExample.Model;
+using Microsoft.AspNetCore.Mvc;
+using Nest;
+
+namespace ElasticSearchExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        private static readonly ConnectionSettings connSettings =
+        new ConnectionSettings(new Uri("http://localhost:9200/"))
+                        .DefaultIndex("esearchitems")
+                        .DefaultMappingFor<Item>(m => m
+                        .PropertyName(p => p.Id, "id")
+            );
+
+        private static readonly ElasticClient elasticClient = new ElasticClient(connSettings);
+        public ItemsController()
+        {
+
+            if (!elasticClient.Indices.Exists("esearchitems").Exists)
+            {
+                elasticClient.Indices.Create("esearchitems",
+                     index => index.Map<Item>(
+                          x => x
+                         .AutoMap()
+                  ));
+
+                elasticClient.Bulk(b => b
+                  .Index("esearchitems")
+                  .IndexMany(ItemService.GetItems())
+                   );
+            }
+        }
+
+        [HttpGet]
+        public List<Item> Get()
+        {
+            var response = elasticClient.Search<Item>(i => i
+           .Query(q => q.MatchAll())
+           .PostFilter(f => f.Range(r => r.Field(fi => fi.Price).GreaterThan(1)))
+            );
+            List<Item> items = new List<Item>();
+            foreach (var item in response.Documents)
+                items.Add(item);
+            return items;
+        }
+
+        [HttpGet("GetByName/{Name}")]
+        public List<Item> GetByName(string Name)
+        {
+            var response = elasticClient.Search<Item>(s => s
+            .Query(q => q.MatchAll())
+            .PostFilter(pf => pf
+            .Term(t => t
+            .Field(f => f.Name)
+            .Value(Name)
+            ))
+            );
+            List<Item> items = response.Documents.ToList();
+            return items;
+        }
+
+    }
+}
